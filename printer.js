bc94126436916ae3191bd39592ab407a8bdde34f
Martin Kruitz <info@kruitzit.nl>
2018-06-17 00:31:13 +0200

    Cleanup logging statement

--- printer.js
+++ printer.js
@@ -1,12 +1,11 @@
 function Printer() {
   return {
     print: function(grid) {
-      console.log(grid);
-
       const gridElm = newElm('grid', function(elm){
         elm.classList.add('size-' + grid.size);
         elm.style = 'width: ' + grid.size + 'em; height: ' + grid.size + 'em;';
       });
+
       for(let i = 0, l = grid.size; i < l; ++i) {
         for(let j = 0, jl = grid.size; j < jl; ++j) {
           let cell = grid.cells[i][j];
