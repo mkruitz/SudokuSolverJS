2a6182d4c6d93b130d94703e2982d18080d9d1d9
Martin Kruitz <info@kruitzit.nl>
2018-06-17 14:56:06 +0200

    Implemented strategy, 'if option only in one group on intersect remove option in other group'

--- strategies/if-option-only-in-one-group-on-intersect-remove-option-in-other-group.js
+++ strategies/if-option-only-in-one-group-on-intersect-remove-option-in-other-group.js
@@ -1,7 +1,48 @@
 function Strategy_IfOptionOnlyInOneGroupOnIntersectRemoveOptionInOtherGroup(grid) {
   return {
     tick: function() {
-      console.log('tickPerGrid');
+      H.loop(grid.intersects, function(intersect) {
+        let intersectCounts = S.countOptions(intersect.intersect);
+        let areaCounts = S.countOptions(intersect.area);
+        let lineCounts = S.countOptions(intersect.line);
+
+        H.loopProps(intersectCounts, function (countValue) {
+          let intersectCount = intersectCounts[countValue];
+          let areaCount = areaCounts[countValue] || [];
+          let lineCount = lineCounts[countValue] || [];
+
+          removeOptions(countValue, intersectCount, areaCount, lineCount);
+          removeOptions(countValue, intersectCount, areaCount, lineCount);
+        });
+      });
     }
   };
+
+  function removeOptions(countValue, intersectCount, groupA, groupB) {
+    if(intersectCount.length  === groupA.length) {
+      let nonIntersecting = getNonIntersectingCells(intersectCount, groupB);
+      let optionsToRemove = [countValue];
+      H.loop(nonIntersecting, function(cell) {
+        S.removeOptionsFromCell(cell, optionsToRemove);
+      });
+    }
+  }
+
+  function getNonIntersectingCells(intersect, group) {
+    const nonIntersect = [];
+    const intersectIds = {};
+
+    H.loop(intersect, function(cell) {
+      intersectIds[cell.id] = true;
+    });
+
+    for(let i = 0, l = group.length; i < l; ++i) {
+      let cell = group[i];
+      if(!intersectIds[cell.id]) {
+        nonIntersect.push(cell);
+      }
+    }
+
+    return nonIntersect;
+  }
 }
